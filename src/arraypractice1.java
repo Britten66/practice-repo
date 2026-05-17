fa3a49a7a3c32e1beb17b8df4412ea6cb0e010dc
Chris <Britten63@hotmail.com>
2026-05-17 16:59:05 -0300

    program created, array takes user input, loops through finds average and sum

--- src/arraypractice1.java
+++ src/arraypractice1.java
@@ -19,7 +19,17 @@
 /** notes:
  * Scanner will be required to take user input. Importing scanner and call it to read from what the user types " in "
  *
- *Prompting the user
+ *Prompting the user to enter a number ( this will select the length of the array )
+ *
+ *once user enters index picked, we will have to make an array that will manage to hold that value
+ * using double for that part
+ *
+ * once the array is created
+ * the loop will iterate through this creation based on what the user enters
+ *
+ * here is a source I used for figuring out the best approach finding average with for loop
+ * https://www.geeksforgeeks.org/dsa/program-to-find-the-average-of-two-numbers/
+ *
  *
  *
  */
@@ -31,7 +41,37 @@ public class arraypractice1 {
 
     public static void main(String[] args) {
 
-    Scanner input = Scanner(System.in)
+    Scanner input = new Scanner(System.in)
+
+        System.out.println("Enter Temp's here ");
+         int days = input.nextint();
+
+         double[] temps = new double[days];
+
+
+                 for(int i = 0; i < days; i++){
+                     System.out.println("Enter Temp for day 1" + (i + 1) + " ");
+                     temps[i] = input.nextDouble;
+                 }
+
+            double average = sum/days;
+
+                 int totalCount = 0;
+                 for(int i = 0; i < days; i++) {
+                     if (temps[i] > average) {
+                         totalCount++;
+                     }
+
+                 }
+                     // this will be where the program will print everything out
+
+
+                     System.out.println("AVG TEMP: " + average);
+                     System.out.println("DAYS ABOVE AVG TEMP: " + totalCount);
+
+
+                    //closing scanner as a good habit
+                    input.close;
 
 
     }
