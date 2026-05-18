ffb637dceefe1370cf2a3b1adf399d1b34ace303
Chris <Britten63@hotmail.com>
2026-05-18 14:55:09 -0300

    file added for project 2 movie theatre

---
+++ src/MovieTheatre.java
@@ -0,0 +1,42 @@
+
+
+
+// Author: Christopher Britten
+// Date Started: 2026-05-13
+// Due By: 2026-05-16
+// Course: DSA
+
+//Instructions
+//        You are required to simulate a movie theater seating system using a 2D array in Java. Each element of the array will represent a seat in the theater.
+//
+//        Your program should be able to:
+//
+//        1. Display the seating chart – Show the initial arrangement of seats (all available at the start).
+//
+//        2. Reserve a seat – Allow a user to book a specific seat.
+//
+//              1. If the seat is already taken, inform the user and suggest an available seat instead.
+//
+//        3. Cancel a reservation – Allow a user to cancel a previously reserved seat and mark it as available again.
+//
+//        4. Retrieve updated seating chart – After any operation, display the current state of the seating chart.
+
+
+/**
+ * NOTES
+ * using a 2D array for this problem ( due to our classes reenforcing the use of rows and columns )
+ *
+ *  decided to use char and not boolean for planning as its going to be printed regardless I wont need to handle T/F
+ *
+ *
+
+**/
+
+
+
+public class MovieTheatre {
+
+CONST rows = 5;
+CONST cols = 6
+
+}
